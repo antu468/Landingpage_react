ba15d14d19b07547106ae86092d30babc4e865fb
antu468 <antudas468@gmail.com>
2025-12-20 15:43:14 +0600

    Star-images isnot fix in bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import './App.css'
 import bg1 from './assets/images/image 85.png';
 import bg2 from './assets/images/image 87.png';
 import logo from './assets/images/Logo (1).png';
+import star1 from './assets/images/image (1).png';
+import star2 from './assets/images/image (2).png';
+import star3 from './assets/images/image (3).png';
+import star4 from './assets/images/image (4).png';
+import star5 from './assets/images/image (5).png';
+
 
 
 
@@ -15,6 +21,12 @@ function App() {
     <>
       <div className="bg-layer bg-87" style={{ backgroundImage: `url(${bg1})` }} />
       <div className="bg-layer bg-85" style={{ backgroundImage: `url(${bg2})` }} />
+        {/* five small star/dust overlays */}
+        <div className="star-layer star-1" style={{ backgroundImage: `url(${star1})` }} />
+        <div className="star-layer star-2" style={{ backgroundImage: `url(${star2})` }} />
+        <div className="star-layer star-3" style={{ backgroundImage: `url(${star3})` }} />
+        <div className="star-layer star-4" style={{ backgroundImage: `url(${star4})` }} />
+        <div className="star-layer star-5" style={{ backgroundImage: `url(${star5})` }} />
       
       <header className="site-header">
         <img src={logo} alt="logo" className="site-logo" />
