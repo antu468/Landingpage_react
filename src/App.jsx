382d38368e8938a41727b693bb27d4119e5273bd
antu468 <antudas468@gmail.com>
2025-12-20 14:37:05 +0600

    logo and nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import bg1 from './assets/images/image 85.png';
 import bg2 from './assets/images/image 87.png';
+import logo from './assets/images/Logo (1).png';
 
 
 
@@ -14,8 +15,22 @@ function App() {
     <>
       <div className="bg-layer bg-87" style={{ backgroundImage: `url(${bg1})` }} />
       <div className="bg-layer bg-85" style={{ backgroundImage: `url(${bg2})` }} />
-
       
+      <header className="site-header">
+        <img src={logo} alt="logo" className="site-logo" />
+      </header>
+      <button className="site-login">Login</button>
+      <nav className="nav-ul">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+
+      </nav >
+
+
 
     </>
   );
