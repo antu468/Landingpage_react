f4a582a02fc98c287e2159ffcc085f4a6485e854
antu468 <antudas468@gmail.com>
2025-12-20 17:13:05 +0600

    Frame add after nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import './App.css'
 import bg1 from './assets/images/image 85.png';
 import bg2 from './assets/images/image 87.png';
 import logo from './assets/images/Logo (1).png';
+import avatar1 from './assets/images/Container (4).png';
+import avatar2 from './assets/images/Container (3).png';
+import avatar3 from './assets/images/Container (2).png';
+import thumb1 from './assets/images/Container (1).png';
+import thumb2 from './assets/images/Container.png';
 import star1 from './assets/images/image (1).png';
 import star2 from './assets/images/image (2).png';
 import star3 from './assets/images/image (3).png';
@@ -32,6 +37,22 @@ function App() {
       <header className="site-header">
         <img src={logo} alt="logo" className="site-logo" />
       </header>
+      {/* client/frame badge (5 images, 5 stars, paragraph) */}
+      <div className="client-frame">
+        <div className="client-avatars">
+          <img src={avatar1} alt="a1" />
+          <img src={avatar2} alt="a2" />
+          <img src={avatar3} alt="a3" />
+          <img src={thumb1} alt="a4" />
+          <img src={thumb2} alt="a5" />
+        </div>
+        <div className="client-meta">
+          <div className="client-stars" aria-hidden>
+            <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+          </div>
+          <p className="client-text">115+ happy clients</p>
+        </div>
+      </div>
       <button className="site-login">Login</button>
       <nav className="nav-ul">
         <ul>
